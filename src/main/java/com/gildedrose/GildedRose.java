41c5a74a7a2bbb8c3e6a04da1a66441dcceb78fe
isoubedda <oubeddaissam@gmail.com>
2019-10-20 17:23:06 +0200

    Refactoring 7 : extraire "Sulfuras, Hand of Ragnaros" dans un if et car il est un objet légendaire il n'a pas de date de péremption et ne perd jamais en qualité danc le if sera vide

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -51,24 +51,19 @@ class GildedRose {
                     item.quality = 0;
                 }
             }else{
-                if (item.quality > 0) {
-                    if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    } else {
+                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+
+                }else {
+                    if (item.quality > 0) {
                         item.quality = item.quality - 1;
                     }
-                }
 
-                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                } else {
                     item.sellIn = item.sellIn - 1;
-                }
 
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            return;
+                    if (item.sellIn < 0) {
+                        if (item.quality > 0) {
+                            item.quality = item.quality - 1;
                         }
-                        item.quality = item.quality - 1;
                     }
                 }
             }
